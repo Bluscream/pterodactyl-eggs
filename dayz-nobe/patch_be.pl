495b2689e8ed610db69d490f82b4d583a86751ff
Bluscream <bluscream@users.noreply.github.com>
2026-09-12 19:00:31 +0200

    feat: automatically enforce battleye = 0 in serverDZ.cfg via patch script and egg parser

--- dayz-nobe/patch_be.pl
+++ dayz-nobe/patch_be.pl
@@ -3,13 +3,40 @@ use strict;
 use warnings;
 
 my $target = $ARGV[0] // "DayZServer";
+
+# --- 1. Enforce No-BE in serverDZ.cfg ---
+if (-f "serverDZ.cfg") {
+    open(my $in, "<", "serverDZ.cfg");
+    my @lines = <$in>;
+    close($in);
+
+    my $found_be = 0;
+    my $found_sig = 0;
+    for (@lines) {
+        if (/^\s*battleye\s*=/i) {
+            $_ = "battleye = 0;\n";
+            $found_be = 1;
+        }
+        if (/^\s*verifySignatures\s*=/i) {
+            # Ensure signatures don't conflict with patched clients
+            $found_sig = 1;
+        }
+    }
+    push @lines, "battleye = 0;\n" unless $found_be;
+
+    open(my $out, ">", "serverDZ.cfg");
+    print $out @lines;
+    close($out);
+}
+
+# --- 2. Patch Binary Memory/Signatures ---
 exit 0 unless -f $target;
 
 open(my $fh, "+<:raw", $target) or exit 0;
 read($fh, my $buf, -s $target);
 
-# Check if already patched
-if (substr($buf, 0x11e9e10, 3) eq "\xb0\x01\xc3") {
+# Check if already patched (quick check for Linux)
+if (length($buf) > 0x11e9e13 && substr($buf, 0x11e9e10, 3) eq "\xb0\x01\xc3") {
     close($fh);
     exit 0;
 }
